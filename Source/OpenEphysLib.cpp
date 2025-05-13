6fb04a2fe5a1076f39af000b8bac21f7a42b89b7
Anjal Doshi <anjaldoshi09@gmail.com>
2025-05-12 19:10:20 -0700

    Bump plugin version to 1.0.0

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -33,7 +33,7 @@ extern "C" EXPORT void getLibInfo (Plugin::LibraryInfo* info)
 {
     info->apiVersion = PLUGIN_API_VER;
     info->name = "Probe Viewer";
-    info->libVersion = "0.4.2";
+    info->libVersion = "1.0.0";
     info->numPlugins = NUM_PLUGINS;
 }
 
